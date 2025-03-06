9ca365399d025c435101419912edb9514cb38bf0
Frida Alexandra Bogacheva <agirre_miron@mail.ru>
2025-03-06 20:17:57 +0300

    first

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import GamePlay from './hole-game/GamePlay';
+import GameController from './hole-game/GameController';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gamePlay = new GamePlay(document.querySelector('.hole-game'));
+  const gameCtrl = new GameController(gamePlay);
+  gameCtrl.start();
+});
